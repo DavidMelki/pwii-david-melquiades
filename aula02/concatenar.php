6043def94eb7c6c750aa34feb90c6ba01823b4fb
David Melquíades <davidmelki08@gmail.com>
2025-02-27 07:35:55 -0300

    Aula de concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,16 @@
+<?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+
+    $nome = "Daniel";
+    $sobrenome = "Lasinskas";
+    $numero = 4;
+
+    echo $nome . "lindo" . "<br>";
+    echo "${nome} é um lindo nome <h1> ${sobrenome} </h1>";
+    echo $numero . "<p>" . $nome . 
+    " " . $sobrenome . "</p>"
+
+?>
