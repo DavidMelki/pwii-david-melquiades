128ba6f73029aa8dec3c9429985224a2a05979d6
David Melquíades <davidmelki08@gmail.com>
2025-04-09 10:33:01 -0300

    Aula sobre funçao e reparar aula 03

---
+++ aula 05/funcao.php
@@ -0,0 +1,15 @@
+<?php
+    function myMessage() {
+        echo "Hello world! <br>";
+    }
+    
+    myMessage();
+    myMessage();
+
+    function Soma($n1,$n2){
+        echo $n1 + $n2, "<br>";
+    }
+
+    Soma(3, 14);
+    Soma(3, 10);
+    ?>
